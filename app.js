a90e56b3c5a82e2dee9a26a34abc32efdf745033
pedro henrique <pedro.avare01@gmail.com>
2020-08-18 21:24:01 -0300

    Adds tours get route

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
+const fs = require('fs');
 const express = require('express');
-
 const app = express();
 
-app.get('/', (request, response) => {
-  response.status(200).json({
-    message: 'Hello from the server side!',
-    app: 'Natours',
-  });
-});
+// app.get('/', (request, response) => {
+//   response.status(200).json({
+//     message: 'Hello from the server side!',
+//     app: 'Natours',
+//   });
+// });
 
-app.post('/', (request, response) => {
-  response.status(200).send('You can post to this endpoint!');
+// app.post('/', (request, response) => {
+//   response.status(200).send('You can post to this endpoint!');
+// });
+
+// Route handler => Callback
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (request, response) => {
+  response.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: { tours },
+  });
 });
 
 const port = 3000;
